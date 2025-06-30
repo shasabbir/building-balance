24bb741ecc87996ebfa7843a3471b3f7296127c9
Sabbir Hossain <shasabbir234@gmail.com>
2025-06-30 11:47:38 +0000

    link=https://script.google.com/macros/s/AKfycbwTNeaM6DcBQ7BKfyq6CaKgcIBQ

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,4 @@
 // 4. In the dialog, for "Who has access", select "Anyone".
 // 5. Click "Deploy". Authorize the script if prompted.
 // 6. Copy the "Web app URL" and paste it below.
-export const API_URL = "YOUR_GOOGLE_APPS_SCRIPT_URL_HERE";
+export const API_URL = "https://script.google.com/macros/s/AKfycbwTNeaM6DcBQ7BKfyq6CaKgcIBQRKOv2EF7YTNkmnPbiszQO61_1heHrL4qSpYM9UpJUg/exec";
