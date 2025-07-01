b57acc41d488ada77b7402bd599eff306f9ebc17
Sabbir Hossain <shasabbir234@gmail.com>
2025-07-01 06:20:27 +0000

    684-0f2405717f96a192.js:1
               GET http

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/building-balance',
   trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
